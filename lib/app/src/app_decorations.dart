26fb12dd5c24d4e5e7970ab51083e1b67afa4523
Yin Wai Naing <71938108+wai72@users.noreply.github.com>
2024-11-17 10:56:27 +0630

    project setup

---
+++ lib/app/src/app_decorations.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class AppDecorations {
+  static BoxDecoration gradientDecoration = const BoxDecoration(
+      gradient: LinearGradient(
+          begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: []));
+}
